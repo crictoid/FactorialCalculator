5dbbe264b86362ade22813f8de330e1197157aa3
Mark David Bulmer <mdbulmer86@gmail.com>
2023-04-30 18:14:54 -0400

    Add files via upload

---
+++ BulmerCh6CPA6/frmFactorial.Designer.vb
@@ -0,0 +1,250 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmFactorial
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn10 = New System.Windows.Forms.Button()
+        Me.btn11 = New System.Windows.Forms.Button()
+        Me.btn12 = New System.Windows.Forms.Button()
+        Me.mnuFitness = New System.Windows.Forms.MenuStrip()
+        Me.mnuFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuClear = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuFitness.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(26, 186)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(75, 23)
+        Me.btn1.TabIndex = 0
+        Me.btn1.Text = "1!"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'lblHeading
+        '
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Font = New System.Drawing.Font("Microsoft Yi Baiti", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.Location = New System.Drawing.Point(12, 9)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(361, 48)
+        Me.lblHeading.TabIndex = 1
+        Me.lblHeading.Text = "Factorial Calculator"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Yi Baiti", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(91, 81)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(193, 19)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Choose a factorial number:"
+        '
+        'btn2
+        '
+        Me.btn2.Location = New System.Drawing.Point(160, 186)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(75, 23)
+        Me.btn2.TabIndex = 3
+        Me.btn2.Text = "2!"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Location = New System.Drawing.Point(304, 186)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(75, 23)
+        Me.btn3.TabIndex = 4
+        Me.btn3.Text = "3!"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Location = New System.Drawing.Point(26, 215)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(75, 23)
+        Me.btn4.TabIndex = 5
+        Me.btn4.Text = "4!"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Location = New System.Drawing.Point(160, 215)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(75, 23)
+        Me.btn5.TabIndex = 6
+        Me.btn5.Text = "5!"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Location = New System.Drawing.Point(304, 215)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(75, 23)
+        Me.btn6.TabIndex = 7
+        Me.btn6.Text = "6!"
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn7
+        '
+        Me.btn7.Location = New System.Drawing.Point(26, 244)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(75, 23)
+        Me.btn7.TabIndex = 8
+        Me.btn7.Text = "7!"
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Location = New System.Drawing.Point(160, 244)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(75, 23)
+        Me.btn8.TabIndex = 9
+        Me.btn8.Text = "8!"
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Location = New System.Drawing.Point(304, 244)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(75, 23)
+        Me.btn9.TabIndex = 10
+        Me.btn9.Text = "9!"
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btn10
+        '
+        Me.btn10.Location = New System.Drawing.Point(26, 273)
+        Me.btn10.Name = "btn10"
+        Me.btn10.Size = New System.Drawing.Size(75, 23)
+        Me.btn10.TabIndex = 11
+        Me.btn10.Text = "10!"
+        Me.btn10.UseVisualStyleBackColor = True
+        '
+        'btn11
+        '
+        Me.btn11.Location = New System.Drawing.Point(160, 273)
+        Me.btn11.Name = "btn11"
+        Me.btn11.Size = New System.Drawing.Size(75, 23)
+        Me.btn11.TabIndex = 12
+        Me.btn11.Text = "11!"
+        Me.btn11.UseVisualStyleBackColor = True
+        '
+        'btn12
+        '
+        Me.btn12.Location = New System.Drawing.Point(304, 273)
+        Me.btn12.Name = "btn12"
+        Me.btn12.Size = New System.Drawing.Size(75, 23)
+        Me.btn12.TabIndex = 13
+        Me.btn12.Text = "12!"
+        Me.btn12.UseVisualStyleBackColor = True
+        '
+        'mnuFitness
+        '
+        Me.mnuFitness.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFile, Me.mnuClear, Me.mnuExit})
+        Me.mnuFitness.Location = New System.Drawing.Point(0, 0)
+        Me.mnuFitness.Name = "mnuFitness"
+        Me.mnuFitness.Size = New System.Drawing.Size(402, 24)
+        Me.mnuFitness.TabIndex = 14
+        Me.mnuFitness.Text = "MenuStrip1"
+        '
+        'mnuFile
+        '
+        Me.mnuFile.Name = "mnuFile"
+        Me.mnuFile.Size = New System.Drawing.Size(37, 20)
+        Me.mnuFile.Text = "&File"
+        '
+        'mnuClear
+        '
+        Me.mnuClear.Name = "mnuClear"
+        Me.mnuClear.Size = New System.Drawing.Size(46, 20)
+        Me.mnuClear.Text = "&Clear"
+        '
+        'mnuExit
+        '
+        Me.mnuExit.Name = "mnuExit"
+        Me.mnuExit.Size = New System.Drawing.Size(38, 20)
+        Me.mnuExit.Text = "E&xit"
+        '
+        'frmFactorial
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.BulmerCh6CPA6.My.Resources.Resources.calculator
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(402, 415)
+        Me.Controls.Add(Me.mnuFitness)
+        Me.Controls.Add(Me.btn12)
+        Me.Controls.Add(Me.btn11)
+        Me.Controls.Add(Me.btn10)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblHeading)
+        Me.Controls.Add(Me.btn1)
+        Me.Name = "frmFactorial"
+        Me.Text = "Factorial Calculator"
+        Me.mnuFitness.ResumeLayout(False)
+        Me.mnuFitness.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn1 As Button
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn10 As Button
+    Friend WithEvents btn11 As Button
+    Friend WithEvents btn12 As Button
+    Friend WithEvents mnuFitness As MenuStrip
+    Friend WithEvents mnuFile As ToolStripMenuItem
+    Friend WithEvents mnuClear As ToolStripMenuItem
+    Friend WithEvents mnuExit As ToolStripMenuItem
+End Class
---
+++ BulmerCh6CPA6/frmFactorial.vb
@@ -0,0 +1,163 @@
+﻿' Program Name:     Factorial Calculator
+' Author:           Mark Bulmer
+' Date:             3/27/2022
+' Purpose:          The Factorial Calculator program offers numbers 1-12
+'                   for the user to select to obtain that numbers' factorial
+'                   power displayed in a pop-up box.
+
+Option Strict On
+
+' I couldn't figure out how to do the proper calculations to apply to a factorial and display it in a listbox,
+' but I found an example which applied the calculation to a button and so I have applied what I could for maybe
+' getting some partial credit.
+Public Class frmFactorial
+
+    ' Each button calls a Factorial function which applies the appropriate number to be computed.
+    Private Sub btn1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn1.Click
+
+        MessageBox.Show(Factorial("1"))
+
+    End Sub
+
+    Private Sub btn2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn2.Click
+
+        MessageBox.Show(Factorial("2"))
+
+    End Sub
+
+    Private Sub btn3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn3.Click
+
+        MessageBox.Show(Factorial("3"))
+
+    End Sub
+
+    Private Sub btn4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn4.Click
+
+        MessageBox.Show(Factorial("4"))
+
+    End Sub
+
+    Private Sub btn5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn5.Click
+
+        MessageBox.Show(Factorial("5"))
+
+    End Sub
+
+    Private Sub btn6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn6.Click
+
+        MessageBox.Show(Factorial("6"))
+
+    End Sub
+
+    Private Sub btn7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn7.Click
+
+        MessageBox.Show(Factorial("7"))
+
+    End Sub
+
+    Private Sub btn8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn8.Click
+
+        MessageBox.Show(Factorial("8"))
+
+    End Sub
+
+    Private Sub btn9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn9.Click
+
+        MessageBox.Show(Factorial("9"))
+
+    End Sub
+
+    Private Sub btn10_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn10.Click
+
+        MessageBox.Show(Factorial("10"))
+
+    End Sub
+
+    Private Sub btn11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn11.Click
+
+        MessageBox.Show(Factorial("11"))
+
+    End Sub
+
+    Private Sub btn12_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn12.Click
+
+        MessageBox.Show(Factorial("12"))
+
+    End Sub
+
+    ' I had trouble figuring this out, and even by checking other examples I could find online I had a lot of trouble 
+    ' making sense of it. Normally I would edit code to suit my needs and change appropriate variables, but in the interest
+    ' of full disclosure I am going to leave this code as-is and attribute it to John Wein of the Microsoft Developer
+    ' Network forums (https://social.msdn.microsoft.com/Forums/en-US/bb23b85f-9a88-4dae-a54e-df71cd25b59d/500-factorial-algorithm-?forum=vbgeneral)
+    Private Function Factorial(ByVal ofThisStringsValue As String) As String
+
+        Dim answer As String = "1"
+        Dim loopEnd As Decimal
+        Decimal.TryParse(ofThisStringsValue, loopEnd)
+        loopEnd = Int(loopEnd)
+        If loopEnd = 0 Then Return "You tried zero or some other text!!"
+        If loopEnd = 1 Then Return "1"
+        Dim start As Decimal = 2
+
+        Do
+            answer = Multiply(answer, start.ToString)
+            start += 1
+        Loop Until start > loopEnd
+
+        Return answer
+    End Function
+
+    Public Function Multiply(ByVal S1 As String, ByVal S2 As String) As String
+
+        Dim A(65535) As Byte
+        Dim B(65535) As Byte
+        Dim C(65535) As Byte
+        Dim I, J, K, IJ, L1, L2 As Int32
+        L1 = S1.Length - 1
+        For I = 0 To L1
+            A(I) = CType(S1.Substring(L1 - I, 1), Byte)
+        Next
+        L2 = S2.Length - 1
+        For J = 0 To L2
+            B(J) = CType(S2.Substring(L2 - J, 1), Byte)
+        Next
+        For I = 0 To L1
+            For J = 0 To L2
+                IJ = I + J
+                C(IJ) += A(I) * B(J)
+                K = 0
+                While C(IJ + K) > 9
+                    C(IJ + 1) += CByte(Fix(C(IJ) / 10))
+                    C(IJ) = CByte(C(IJ) Mod 10)
+                    K += 1
+                End While
+            Next
+        Next
+        Dim S As String = ""
+        L1 += L2 + 2
+        For I = L1 To 0 Step -1
+            S += C(I).ToString
+        Next
+        'Remove any character "0" 's from the start of the string        
+        'until a non-zero numeric character is reached.        
+        Dim testChr As String = ""
+        Do
+            testChr = S.Substring(0, 1)
+            If testChr = "0" Then
+                S = S.Remove(0, 1)
+            Else
+                Exit Do
+            End If
+        Loop
+        Return S
+    End Function
+    Private Sub mnuClear_Click(sender As Object, e As EventArgs) Handles mnuClear.Click
+
+    End Sub
+
+    Private Sub mnuExit_Click(sender As Object, e As EventArgs) Handles mnuExit.Click
+        ' The mnuExit click event closes the window and exits the application
+
+        Close()
+    End Sub
+End Class
